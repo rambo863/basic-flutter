0e71ed9bbbe57393b482665a1bc62b8bdc644820
rambo864 <72152045+rambo864@users.noreply.github.com>
2022-10-08 15:24:30 +0530

    Update constvariable.dart

--- constvariable.dart
+++ constvariable.dart
@@ -8,3 +8,29 @@ const collectionOfDoubles = [14.55, cost]; // okay because "cost" is also a cons
 // not allowed!
 // the compiler can't know this width until the function is called.
  const screenWidth = getScreenWidth();
+
+main() {
+	
+// Assigning value to var1
+// variable without datatype
+final var1 = 12;
+print(var1);
+	
+// Assigning value to var2
+// variable with datatype
+final String var2 = "GeeksForGeeks";
+print(var2);
+}
+
+main() { 
+      
+  // Assigning value to var1 
+  // variable without datatype 
+  const var1 = 32;   
+  print(var1); 
+       
+  // Assigning value to var2 
+  // variable with datatype 
+  const String var2 = "GeeksForGeeks but with const";   
+  print(var2); 
+}
